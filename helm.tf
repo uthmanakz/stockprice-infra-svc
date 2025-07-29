977dd402144188a685e294ce3fd143783108d9d7
Uthman Akinwumi <uthmanakz@Uthmans-MacBook-Pro.local>
2025-07-29 16:15:16 +0100

    edited helm.tf config

--- helm.tf
+++ helm.tf
@@ -1,3 +1,21 @@
+terraform {
+  required_providers {
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.0.0"
+    }
+  }
+}
+
+provider "helm" {
+  kubernetes = {
+    host                   = data.terraform_remote_state.eks.outputs.eks_cluster_endpoint
+    cluster_ca_certificate = base64decode(data.terraform_remote_state.eks.outputs.eks_cluster_ca)
+    token                  = data.aws_eks_cluster_auth.cluster.token
+  }
+}
+
+
 resource "helm_release" "aws_load_balancer_controller" {
   name       = "aws-load-balancer-controller"
   namespace  = "kube-system"
@@ -5,30 +23,28 @@ resource "helm_release" "aws_load_balancer_controller" {
   chart      = "aws-load-balancer-controller"
   version    = "1.13.0"
 
-  set {
-    name  = "clusterName"
-    value = stockprice
-  }
-
-  set {
-    name  = "region"
-    value = eu-west-2
-  }
-
-  set {
-    name  = "vpcId"
-    value = data.terraform_remote_state.eks.outputs.vpc_id
-  }
-
-  set {
-    name  = "serviceAccount.create"
-    value = "false"
-  }
-
-  set {
-    name  = "serviceAccount.name"
-    value = "aws-load-balancer-controller"
-  }
+  set = [
+    {
+      name  = "clusterName"
+      value = "stockprice"
+    },
+    {
+      name  = "region"
+      value = "eu-west-2"
+    },
+    {
+      name  = "vpcId"
+      value = data.terraform_remote_state.eks.outputs.vpc_id
+    },
+    {
+      name  = "serviceAccount.create"
+      value = "false"
+    },
+    {
+      name  = "serviceAccount.name"
+      value = "aws-load-balancer-controller"
+    }
+  ]
 
   depends_on = [kubernetes_service_account.alb_sa]
 }
--- output.tf
+++
@@ -1,3 +0,0 @@
-output "alb_irsa_role_arn" {
-  value = module.ingress_irsa.iam_role_arn
-}
