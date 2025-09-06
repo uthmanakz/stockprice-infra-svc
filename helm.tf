a588aafd05e5171b20cc5a72f2f18eacc90a3c51
Uthman Akinwumi <uthmanakz@Uthmans-MacBook-Pro.local>
2025-09-06 08:21:35 +0100

    added argocd helm release

--- helm.tf
+++ helm.tf
@@ -56,6 +56,7 @@ resource "helm_release" "argocd" {
   repository = "https://argoproj.github.io/argo-helm"
   chart      = "argo-cd"
   version    = "5.51.6" # Check for latest: https://artifacthub.io/packages/helm/argo/argo-cd
+  create_namespace = true
 
   set = [
     {
